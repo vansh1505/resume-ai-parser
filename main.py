a74200fd127691921f16f536bfd82bd07487bf3d
Vansh <86408597+vansh1505@users.noreply.github.com>
2025-08-07 21:44:27 +0530

    testing

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ origins = ["https://uniplace.vercel.app"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins="*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
